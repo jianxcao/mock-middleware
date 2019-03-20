8b6705cd87e4661a3810e0b61565f919be404c10
jxcao <jxcao@corp.netease.com>
2019-03-20 15:17:59 +0800

    feat: mock demo

--- demo/mock/index.js
+++ demo/mock/index.js
@@ -1,40 +1,42 @@
 module.exports = {
-    // Support type as Object and Array
-    'GET /api/users': {
-      users: [1, 2]
-    },
-    
-    'GET /api/params': (req, res, next) => {
-      console.log(req.query);
-      res.body = {
-        id: 225672
-      }
-    },
-    // Method like GET or POST can be omitted
-    '/api/users/1': {
-      id: 1
-    },
-    'GET /api/uses/45': {
-      code: 0,
-      data: [],
-      msg: '323'
-    },
-    'GET /api/:id': (req, res, next) => {
-      res.body = {
-        id: req.params.id
-      };
-  
-    },
-  
-    'POST /api/users': (req, res, next) => {
-      res.body = {
-        id: 222
-      }
-    },
-    'POST /api/other': (req, res, next) => {
-      res.body = {
-        id: 222
-      }
-    }
-  };
-  
+  // Support type as Object and Array
+  'GET /api/users': {
+    users: [1, 2],
+  },
+
+  // Method like GET or POST can be omitted
+  '/api/users/1': {
+    id: 1,
+  },
+  'GET /api/uses/45': {
+    code: 0,
+    data: [],
+    msg: '323',
+  },
+  'GET /api/:id': async (req, res) => {
+    res.body = {
+      id: req.params.id,
+    };
+  },
+
+  'POST /api/users': async (req, res) => {
+    await sleep(3000);
+    res.body = {
+      id: 222,
+    };
+  },
+  'POST /api/other': async (req, res) => {
+    await sleep(3000);
+    res.body = {
+      id: 222,
+    };
+  },
+};
+
+function sleep() {
+  return new Promise(function(res, rej) {
+    setTimeout(function() {
+      res();
+    }, 3000);
+  });
+}
