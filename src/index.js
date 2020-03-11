3a3fad63766896f834a70fbfea5f975c9c7d7332
jxcao <jxcao@corp.netease.com>
2020-03-11 15:40:59 +0800

    fix: fix express bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const match = require('./match');
-const multer = require('multer');
-const BODY_PARSED_METHODS = ['post', 'put', 'patch', 'delete'];
-const parse = require('co-body');
-const { noop, isJSON } = require('./utils');
+const match = require("./match");
+const multer = require("multer");
+const BODY_PARSED_METHODS = ["post", "put", "patch", "delete"];
+const parse = require("co-body");
+const { noop, isJSON } = require("./utils");
 
-const bodyDetail = async function (req, res, callback = noop) {
+const bodyDetail = async function(req, res, callback = noop) {
   const method = req.method.toLowerCase();
   if (BODY_PARSED_METHODS.includes(method)) {
     const body = await parse(req);
@@ -13,9 +13,8 @@ const bodyDetail = async function (req, res, callback = noop) {
   }
 };
 
-
-const fileParse = function (req, res, callback = noop) {
-  return new Promise(function (resolve, reject) {
+const fileParse = function(req, res, callback = noop) {
+  return new Promise(function(resolve, reject) {
     multer().any()(req, res, () => {
       callback();
       resolve();
@@ -23,56 +22,57 @@ const fileParse = function (req, res, callback = noop) {
   });
 };
 
-exports.koa = function (opt) {
+exports.koa = function(opt) {
   const mo = match(opt);
-  return async function (ctx, next) {
+  return async function(ctx, next) {
     const result = mo(ctx.request);
     // 匹配到结果
     if (result) {
       const handler = result.handler;
       // req参数解析
       await bodyDetail(ctx.request, ctx.response);
-      if (typeof handler === 'function') {
+      if (typeof handler === "function") {
         await fileParse(ctx.request, ctx.response);
         return await handler(ctx.request, ctx.response, next);
       }
-      return ctx.body = handler;
+      return (ctx.body = handler);
     }
     await next();
   };
 };
 
-exports.koa1 = function (opt) {
+exports.koa1 = function(opt) {
   const mo = match(opt);
-  return function* (next) {
+  return function*(next) {
     const ctx = this;
     const result = mo(ctx.request);
     if (result) {
       let handler = result.handler;
       yield bodyDetail(ctx.request, ctx.response);
-      if (typeof handler === 'function') {
+      if (typeof handler === "function") {
         yield fileParse(ctx.request, ctx.response);
         yield Promise.resolve(handler(ctx.request, ctx.response, next));
         return;
       }
-      return ctx.body = handler;
+      return (ctx.body = handler);
     }
-    yield * next;
+    yield* next;
   };
 };
 
-exports.express = function () {
-  return function (req, res, next) {
+exports.express = function(opt) {
+  const mo = match(opt);
+  return function(req, res, next) {
     const result = mo(req);
     if (result) {
       const handler = result.handler;
       bodyDetail(req, res, () => {
-        if (typeof handler === 'function') {
+        if (typeof handler === "function") {
           fileParse(req, res, () => {
             return handler(req, res, next);
           });
         }
-        return res.json ? res.json(handler) : res.body = handler;
+        return res.json ? res.json(handler) : (res.body = handler);
       });
     } else {
       next();
